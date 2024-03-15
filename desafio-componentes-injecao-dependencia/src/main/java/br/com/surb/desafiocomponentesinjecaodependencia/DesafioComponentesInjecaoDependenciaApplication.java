2ccb8683ec49b4268f55a1d13c3c823ed7d24d94
claudiobelempb <claudio.c.lima@hotmail.com>
2024-03-15 14:21:45 -0300

    DESAFIO: Componentes e injeção de dependência inicializado

---
+++ desafio-componentes-injecao-dependencia/src/main/java/br/com/surb/desafiocomponentesinjecaodependencia/DesafioComponentesInjecaoDependenciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.surb.desafiocomponentesinjecaodependencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioComponentesInjecaoDependenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioComponentesInjecaoDependenciaApplication.class, args);
+	}
+
+}
