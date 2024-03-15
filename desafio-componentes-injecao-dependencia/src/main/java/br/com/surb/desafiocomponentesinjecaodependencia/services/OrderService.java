357be34660427d1b5d23f71ad281d22c7a2c22aa
claudiobelempb <claudio.c.lima@hotmail.com>
2024-03-15 14:31:24 -0300

    add @Service

--- desafio-componentes-injecao-dependencia/src/main/java/br/com/surb/desafiocomponentesinjecaodependencia/services/OrderService.java
+++ desafio-componentes-injecao-dependencia/src/main/java/br/com/surb/desafiocomponentesinjecaodependencia/services/OrderService.java
@@ -1,7 +1,9 @@
 package br.com.surb.desafiocomponentesinjecaodependencia.services;
 
 import br.com.surb.desafiocomponentesinjecaodependencia.entities.Order;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderService {
     public double total(Order order){
         return order.getBasic();
--- desafio-componentes-injecao-dependencia/src/main/java/br/com/surb/desafiocomponentesinjecaodependencia/services/ShippingService.java
+++ desafio-componentes-injecao-dependencia/src/main/java/br/com/surb/desafiocomponentesinjecaodependencia/services/ShippingService.java
@@ -1,7 +1,9 @@
 package br.com.surb.desafiocomponentesinjecaodependencia.services;
 
 import br.com.surb.desafiocomponentesinjecaodependencia.entities.Order;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ShippingService {
     public double shippingService(Order order){
         return order.getBasic() - order.getDiscount();
